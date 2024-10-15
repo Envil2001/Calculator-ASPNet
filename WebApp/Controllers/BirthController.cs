38c35f30816ee44a114e423b597a0656057d6074
ashcherbakov <sherbakov12342@gmail.com>
2024-10-15 16:33:24 +0200

    finished zadanie 2 Lab 2

---
+++ WebApp/Controllers/BirthController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class BirthController : Controller
+{
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Result([FromForm] Birth model)
+    {
+        if (!model.IsValid())
+        {
+            ViewBag.ErrorMessage = "Niepoprawne dane.";
+            return View("Form");
+        }
+
+        ViewBag.Name = model.Name;
+        ViewBag.Age = model.CalculateAge();
+        return View();
+    }
+}
