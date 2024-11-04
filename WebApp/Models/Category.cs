6c85b56d81d4a8f88a3c031acd1d7fb336dc7a90
Envil <sherbakov12342@gmail.com>
2024-11-04 13:59:17 +0100

    feat: added models contact and category

---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public enum Category
+{
+    [Display(Name = "Rodzina", Order = 1)]
+    Family = 1,
+    [Display(Name = "Znajomi", Order = 2)]
+    Friend = 2,
+    [Display(Name = "Kontakt zawodowy", Order = 3)]
+    Business = 3,
+}
---
+++ WebApp/Models/Services/MemoryContactService.cs
@@ -0,0 +1,76 @@
+﻿namespace WebApp.Models.Services;
+
+public class MemoryContactService : IContactService
+{
+    private Dictionary<int, ContactModel> _contacts = new()
+    {
+        {
+            1, new ContactModel()
+            {
+                Id = 1,
+                Category = Category.Business,
+                FirstName = "Adam",
+                LastName = "Abecki",
+                Email = "adam@wsei.edu.pl",
+                BirthDate = new DateOnly(2000, 10, 10),
+                PhoneNumber = "+48 222 222 222"
+            }
+        },
+        {
+            2, new ContactModel()
+            {
+                Id = 1,
+                Category = Category.Family,
+                FirstName = "Ewa",
+                LastName = "Bebecka",
+                Email = "ewa@wsei.edu.pl",
+                BirthDate = new DateOnly(2001, 10, 10),
+                PhoneNumber = "+48 222 222 333"
+            }
+        },
+        {
+            3, new ContactModel()
+            {
+                Id = 3,
+                Category = Category.Family,
+                FirstName = "Boba",
+                LastName = "Bobov",
+                Email = "bob@wsei.edu.pl",
+                BirthDate = new DateOnly(1992, 10, 10),
+                PhoneNumber = "+48 222 222 444"
+            }
+        },
+    };
+
+    private int _index = 3;
+
+    public void Add(ContactModel model)
+    {
+        model.Id = ++_index;
+        _contacts.Add(model.Id, model);
+    }
+
+    public void Update(ContactModel model)
+    {
+        if (_contacts.ContainsKey(model.Id))
+        {
+            _contacts[model.Id] = model;
+        }
+    }
+
+    public void Delete(int id)
+    {
+        _contacts.Remove(id);
+    }
+
+    public List<ContactModel> GetAll()
+    {
+        return _contacts.Values.ToList();
+    }
+
+    public ContactModel? GetById(int id)
+    {
+        return _contacts[id];
+    }
+}
+
