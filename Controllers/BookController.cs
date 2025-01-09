d5086058e01da243e23abc1b824438c8ff19e611
Envil <sherbakov12342@gmail.com>
2025-01-09 01:45:47 +0100

    feat: Add Book model, controller, and views for Laboratorium 3

---
+++ Controllers/BookController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+ public class BookController : Controller
+    {
+        private static Dictionary<int, Book> _books = new Dictionary<int, Book>();
+
+        public IActionResult Index()
+        {
+            return View(_books);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book model)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = _books.Keys.Count != 0 ? _books.Keys.Max() : 0;
+                model.Id = id + 1;
+                _books.Add(model.Id, model);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (_books.ContainsKey(id))
+            {
+                return View(_books[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book model)
+        {
+            if (ModelState.IsValid)
+            {
+                _books[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (_books.ContainsKey(id))
+            {
+                return View(_books[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (_books.ContainsKey(id))
+            {
+                return View(_books[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if (_books.ContainsKey(id))
+            {
+                _books.Remove(id);
+            }
+            return RedirectToAction("Index");
+        }
+    }
---
+++ Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models;
+
+public class Book
+{
+    [HiddenInput]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać tytuł!")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać autora!")]
+    public string Author { get; set; }
+
+    [Range(1, 10000, ErrorMessage = "Liczba stron musi być między 1 a 10000!")]
+    public int PageCount { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać ISBN!")]
+    public string ISBN { get; set; }
+
+    [Range(1900, 2100, ErrorMessage = "Rok wydania musi być między 1900 a 2100!")]
+    public int YearPublished { get; set; }
+
+    [Required(ErrorMessage = "Proszę podać wydawnictwo!")]
+    public string Publisher { get; set; }
+}
